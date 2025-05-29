1efda0020d18683f2d549545429480506c94c5ff
YanicB <yanic.ballez@gmail.com>
2025-05-29 15:05:43 -0400

    added a few compoments to frontend, added tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function App() {
+    return (
+        <div className="flex justify-center bg-green-500">
+            <h1>To Do </h1>
+        </div>
+    )
+}
+export default App

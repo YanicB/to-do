3f2235cf0c821ea7aa05100c691719254ab38fe8
YanicB <yanic.ballez@gmail.com>
2025-06-24 11:58:30 -0400

    added dist so i could push application to website and make live

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cors from 'cors'
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 app.use('/api/v1/todo', todoRouter);
 
 app.get('/', (req, res) => {
--- frontend/src/services/todo.jsx
+++ frontend/src/services/todo.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/v1/todo'
+const baseUrl = 'api/v1/todo'
 
 export const getAll = async () => {
     const response = await axios.get(baseUrl);

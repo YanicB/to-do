3bca90d7e2b685a6b25aeb54be16c6206685cb66
YanicB <yanic.ballez@gmail.com>
2025-06-25 17:42:54 -0400

    added errors in the frontend

--- frontend/src/services/todo.jsx
+++ frontend/src/services/todo.jsx
@@ -2,18 +2,30 @@ import axios from 'axios'
 const baseUrl = 'api/v1/todo'
 
 export const getAll = async () => {
-    const response = await axios.get(baseUrl);
-    return response.data;
+    try {
+        const response = await axios.get(baseUrl);
+        return response.data;
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 export const postTodo = async (todoObject) => {
-    const response = await axios.post(baseUrl, todoObject)
-    return response.data
+    try {
+        const response = await axios.post(baseUrl, todoObject)
+        return response.data
+    } catch (err) {
+        console.log(`Invalid Input: ${err}`)
+    }
 }
 
 export const deleteTodo = async (id) => {
-    const response = await axios.delete(`${baseUrl}/${id}`)
-    return response.data
+    try {
+        const response = await axios.delete(`${baseUrl}/${id}`)
+        return response.data
+    } catch (err) {
+        console.log(err)
+    }
 }
 
 

bef4766c380a71838e2f884d3812254fa40b1849
YanicB <yanic.ballez@gmail.com>
2025-06-25 14:06:14 -0400

    added api endpoints from the backend and the database

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,10 @@
 import express from 'express';
-import todoRouter from './routes/todo.routes.js';
+//import todoRouter from './routes/todo.routes.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import Todo from './models/todo.js'
 
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://todouser:${password}@cluster0.9rfxcrd.mongodb.net/something?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery', false)
-
-mongoose.connect(url)
-const todoSchema = new mongoose.Schema({
-    content: String,
-    completion: Boolean,
-})
-
-const Todo = mongoose.model('Todo', todoSchema)
-
-if (process.argv.length == 5) {
-    const todo = new Todo({
-        content: process.argv[3],
-        completion: process.argv[4].toLowerCase() === 'true',
-    })
-    todo.save().then(result => {
-        console.log(`added ${result.content}, with completion ${result.completion}`)
-        mongoose.connection.close()
-    })
-}
+dotenv.config()
 
 const app = express();
 app.use(express.json());
@@ -41,9 +15,32 @@ app.get('/', (req, res) => {
     res.send('Welcome to the To-do list API');
 });
 
+app.post('/api/v1/todo', (req, res) => {
+    const body = req.body
+
+    if (!body.content) {
+        return response.status(400).json({ error: 'content missing' })
+    }
+
+    const todo = new Todo({
+        content: body.content,
+        completion: body.completion || false,
+    })
+
+    todo.save().then(savedTodo => {
+        res.json(savedTodo)
+    })
+})
+
 app.get('/api/v1/todo', (req, res) => {
     Todo.find({}).then(todos => {
         res.json(todos)
+    })
+
+    app.get('api/v1/todo/:id', (req, res) => {
+        Todo.findById(req.params.id).then(todo => {
+            res.json(todo)
+        })
     })
 })
 
---
+++ backend/models/todo.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
+
+console.log(`connecting to ${url}`)
+mongoose.connect(url)
+    .then(result => {
+        console.log("Connected to MongoDB")
+    })
+    .catch(error => {
+        console.log(`Error found: ${error}`)
+    })
+
+const todoSchema = new mongoose.Schema({
+    content: String,
+    completion: Boolean,
+})
+
+todoSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Todo = mongoose.model('Todo', todoSchema)
+
+export default Todo
+
+

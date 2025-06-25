ac04ed17f9bafbd858fe0bcce38dfefc56226a1f
YanicB <yanic.ballez@gmail.com>
2025-06-25 17:10:28 -0400

    stuff

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 import dotenv from 'dotenv'
 dotenv.config()
-import express from 'express';
+import express from 'express'
 import Todo from './models/todo.js'
 
+const app = express()
 
-const app = express();
-app.use(express.json());
-app.use(express.static('dist'));
-//app.use('/api/v1/todo', todoRouter);
+app.use(express.json())
+app.use(express.static('dist'))
 
+// ROUTES
 app.get('/', (req, res) => {
-    res.send('Welcome to the To-do list API');
-});
+    res.send('Welcome to the To-do list API')
+})
 
-app.post('/api/v1/todo', (req, res) => {
+app.post('/api/v1/todo/:id', (req, res, next) => {
     const body = req.body
     if (!body.content) {
         return res.status(400).json({ error: 'content missing' })
@@ -25,6 +25,7 @@ app.post('/api/v1/todo', (req, res) => {
     todo.save().then(savedTodo => {
         res.json(savedTodo)
     })
+        .catch(err => next(err))
 })
 
 app.get('/api/v1/todo', (req, res) => {
@@ -33,22 +34,56 @@ app.get('/api/v1/todo', (req, res) => {
     })
 })
 
-app.delete('/api/v1/todo/:id', (req, res) => {
-    const id = req.params.id
-    Todo.findByIdAndDelete(id).then(deletedTodo => {
-        res.json({ message: "todo deleted.", todo: deletedTodo })
+app.get('/api/v1/todo/:id', (req, res, next) => {
+    Todo.findById(req.params.id)
+        .then(todo => {
+            if (todo) {
+                res.json(todo)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/v1/todo/:id', (req, res, next) => {
+    const { content, completion } = req.body
+    Todo.findById(req.params.id)
+        .then(todo => {
+            if (!todo) {
+                return res.status(404).end()
+            }
+            todo.content = content
+            todo.completion = completion
+
+            return todo.save().then((updatedTodo) => {
+                res.json(updatedTodo)
+            })
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/v1/todo/:id', (req, res, next) => {
+    Todo.findByIdAndDelete(req.params.id)
+        .then(() => res.status(204).end())
+        .catch(error => next(error))
+})
+
+// ERROR HANDLER
+const errorHandler = (err, req, res, next) => {
+    console.error(err.message)
+    if (err.name === 'CastError') {
+        return res.status(400).send({ error: 'malformatted id' })
+    } else if (err.name === 'ValidationError') {
+        return res.status(400).send({ error: err.message })
     }
-    )
-})
+    next(err)
+}
 
+app.use(errorHandler)
 
-app.get('api/v1/todo/:id', (req, res) => {
-    Todo.findById(req.params.id).then(todo => {
-        res.json(todo)
-    })
-})
-
-const PORT = process.env.PORT || 3001;
+// START SERVER
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Listening on http://localhost:${PORT}`);
-});
+    console.log(`Listening on http://localhost:${PORT}`)
+})
--- backend/models/todo.js
+++ backend/models/todo.js
@@ -16,7 +16,11 @@ mongoose.connect(url)
     })
 
 const todoSchema = new mongoose.Schema({
-    content: String,
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
     completion: Boolean,
 })
 
